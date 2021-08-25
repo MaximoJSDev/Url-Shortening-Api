ec49a3feea6fbf42672a2829a11237ab083ed46a
Maximo-4ever <choripapa04@gmail.com>
2021-08-25 15:04:45 -0500

    implementacion de menu responsive y estilo del short link

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const toggleMenu = document.querySelector(".toggle-menu");
+const modal = document.querySelector(".modal");
+const nav = document.querySelector(".nav");
+const sectionsNav = document.querySelector(".nav__left__links");
+const sectionsNavClone = sectionsNav.cloneNode(true);
+const logging = document.querySelector(".nav__right__links");
+const loggingClone = logging.cloneNode(true);
 
 toggleMenu.addEventListener("click", () => {
-    console.log("WELCOME");
+  modal.classList.toggle("active");
+  modal.appendChild(sectionsNavClone);
+  modal.appendChild(loggingClone);
+});
+
+fetch("https://api.shrtco.de/v2/shorten?url=example.org/very/long/link.html").then(response => {
+    return response
 })
